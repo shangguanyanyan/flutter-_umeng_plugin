1c753ba5ab12a25d479ea744eb3ee76eb776f66f
shangguanyanyan <1024503335@qq.com>
2020-06-23 09:44:26 +0800

    升级iOS端友盟SDK版本

--- ios/Classes/UmengPlugin.m
+++ ios/Classes/UmengPlugin.m
@@ -1,7 +1,7 @@
 #import "UmengPlugin.h"
 #import <UMCommon/UMCommon.h>
 #import <UMCommonLog/UMCommonLogHeaders.h>
-#import <UMAnalytics/MobClick.h>
+#import <UMCommon/MobClick.h>
 #import "UmengPluginMethods.h"
 
 @implementation UmengPlugin
@@ -31,6 +31,7 @@
     BOOL isLogEnable = call.arguments[LOG_ENABLE];
     NSString *channel = call.arguments[CHANNEL];
     NSString *key = call.arguments[KEY];
+
     BOOL encrypt = call.arguments[ENCRYPT];
     NSLog(@"islogEnable:%d,channel:%@,key:%@,encrypt:%d"
       ,isLogEnable,channel,key,encrypt);
@@ -44,17 +45,7 @@
     }
 }
 -(void)customeEvent:(FlutterMethodCall*)call result:(FlutterResult)result{
-    //初始化字典
-    NSDictionary *num = [[NSDictionary alloc] initWithObjectsAndKeys:@"one", @"num1", @"two", @"num2", @"three", @"num3", nil];
-    //注：先写value，再写key，一对key-value是一个元素，nil作为字典存放元素的结束标志。
-
-    //便利构造器
-    NSDictionary *num1 = [NSDictionary dictionaryWithObjectsAndKeys:@"one",@"num1",@"two",@"num2",nil];
-
-    //字面量
-    //使用“ @{} ”，“ {} ”中存入字典元素，key：value一一对应，元素之间使用”，”相隔。
-    NSDictionary *num2 = @{@"num1":@"one",@"num2":@"two"};
-    //NSLog(@"%@,%@,%@",num,num1,num2);
+    
     NSDictionary *param = call.arguments[PARAMS];
     NSLog(@"直接打印call.arguments%@",call.arguments[PARAMS]);
     NSLog(@"打印param%@",param);
